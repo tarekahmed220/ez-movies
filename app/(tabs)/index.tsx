f12a44967ec00ded2963c3ab8ba600189db5c429
Tarek Ahmed <tarek.ahm12@gmail.com>
2026-04-10 15:32:12 +0200

    feat: implement movie fetching functionality and enhance UI with loading/error states

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,28 @@
+import MovieCard from "@/components/movie-card";
 import SearchBar from "@/components/search-bar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import useFetch from "@/hooks/use-fetch";
+import { fetchMovies } from "@/services/api";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import "../global.css";
 
 export default function App() {
   const router = useRouter();
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -19,12 +35,48 @@ export default function App() {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto " />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            placeholder="Search for a movies..."
-            onPress={() => router.push("/search")}
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : error ? (
+          <View className="flex-1 mt-5">
+            <Text className="text-white text-center">
+              Failed to load movies.
+            </Text>
+            <Text className="text-white text-center">
+              {error?.message || "An unexpected error occurred."}
+            </Text>
+          </View>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              placeholder="Search for a movies..."
+              onPress={() => router.push("/search")}
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
   return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4 border border-dark-100">
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-2 border border-dark-100">
       <Image
         source={icons.search}
         className="w-5 h-5"
